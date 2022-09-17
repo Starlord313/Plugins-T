02194b59858e0f4cd1ca57dd162919eb45bf8ffb
Starlord313 <starlordgaming57@gmail.com>
2022-09-17 06:54:24 +0530

    Update hell_string.py

--- hell_string.py
+++ hell_string.py
@@ -8,14 +8,14 @@ from telethon.sync import TelegramClient
 
 
 def main():
-    print("T E A M    W A R B O T   ! !")
-    print("Hello!! Welcome to WarBot Session Generator\n")
+    print("T E A M    A L N B O T   ! !")
+    print("Hello!! Welcome to ALNBot Session Generator\n")
     print("Human Verification Required !!")
     while True:
         verify = int(randint(1, 50))
         okvai = int(input(f"Enter {verify} to continue: "))
         if okvai == verify:
-            print("\nChoose the string session type: \n1. WarBot (Telethon) \n2. Music Bot (Pyrogram) \n3. Instagram Session")
+            print("\nChoose the string session type: \n1. ALNBot (Telethon) \n2. Music Bot (Pyrogram) \n3. Instagram Session")
             while True:
                 library = input("\nYour Choice: ")
                 if library == "1":
@@ -47,19 +47,19 @@ def generate_pyro_session():
 
 
 def generate_telethon_session():
-    print("\nTelethon Session For WarBot!")
+    print("\nTelethon Session For ALNBot!")
     APP_ID = int(input("\nEnter APP ID here: "))
     API_HASH = input("\nEnter API HASH here: ")
     with TelegramClient(StringSession(), APP_ID, API_HASH) as hellbot:
-        print("\nYour WarBot Session Is sent in your Telegram Saved Messages.")
+        print("\nYour ALNBot Session Is sent in your Telegram Saved Messages.")
         hellbot.send_message(
             "me",
-            f"#WARBOT #WARBOT_SESSION #TELETHON \n\n`{hellbot.session.save()}`",
+            f"#ALNBOT #ALNBOT_SESSION #TELETHON \n\n`{hellbot.session.save()}`",
         )
 
 
 def generate_insta_session():
-    print("Instagram Session For WARBot!")
+    print("Instagram Session For ALNBot!")
     cl = IClient()
     username = input("Enter your Instagram Username: ")
     password = input("Enter your Instagram Password: ")
