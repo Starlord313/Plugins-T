935f99b7db926951f66ed9d952907f28a17142f6
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-09-17 08:14:08 +0530

    Update config.py

--- HellConfig/config.py
+++ HellConfig/config.py
@@ -47,8 +47,8 @@ class Config(object):
     LYRICS_API = os.environ.get("LYRICS_API", None)
     MAX_MESSAGE_SIZE_LIMIT = 4095
     MAX_SPAM = int(os.environ.get("MAX_SPAM", 3))
-    MY_CHANNEL = os.environ.get("YOUR_CHANNEL", "ALNVIP)
-    MY_GROUP = os.environ.get("YOUR_GROUP", "chatgroup")
+    MY_CHANNEL = os.environ.get("YOUR_CHANNEL", "alnvip)
+    MY_GROUP = os.environ.get("YOUR_GROUP", "alnchatgrp")
     OCR_API = os.environ.get("OCR_API", None)
     PLUGIN_CHANNEL = os.environ.get("PLUGIN_CHANNEL", None)
     if PLUGIN_CHANNEL:
