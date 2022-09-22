7ab55816cca22565849de21828714595e238be97
Starlord313 <starlordgaming57@gmail.com>
2022-09-22 22:03:46 +0530

    Update session.py

--- TelethonHell/clients/session.py
+++ TelethonHell/clients/session.py
@@ -82,7 +82,7 @@ else:
 
 
 HellBot = TelegramClient(
-    session="War-TBot",
+    session="WAR-TBot",
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
     connection=ConnectionTcpAbridged,
